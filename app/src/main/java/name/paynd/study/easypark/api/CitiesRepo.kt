6389d14f4bcadfcef34309a4feedb4a4cd1fe42f
andrew <a.kupreychik@gmail.com>
2021-11-30 14:29:00 +0100

    mapbox key has been replaced

--- app/src/main/java/name/paynd/study/easypark/api/CitiesRepo.kt
+++ app/src/main/java/name/paynd/study/easypark/api/CitiesRepo.kt
@@ -1,12 +1,10 @@
 package name.paynd.study.easypark.api
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.flow
 
 interface CitiesRepo {
-    val citiesFlow : Flow<List<City>?>
-    val citiesDistances : Flow<List<CityDistance>?>
+    val citiesFlow: Flow<List<City>?>
+    val citiesDistances: Flow<List<CityDistance>?>
 
-    fun loadCity(name: String) : Flow<City?>
+    fun loadCity(name: String): Flow<City?>
 }
